6a5a79888232623bc48b77913c074a8580829a8f
WindrunnerMax <651525974@qq.com>
2024-03-01 23:37:20 +0800

    feat: complete grades

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,14 +1,18 @@
 export const RegExec = {
   exec: (regex: RegExp, s: string, index = 1): string => {
     const res = regex.exec(s);
-    return res ? res[index] || res[0] : "";
+    if (!res) return "";
+    return res[index] !== void 0 ? res[index] : res[0];
   },
   match: (regex: RegExp, s: string): Array<string> => {
     const result: string[] = [];
     let temp: RegExpExecArray | null = null;
     const flags = `${regex.flags}${regex.global ? "" : "g"}`;
     regex = new RegExp(regex, flags);
-    while ((temp = regex.exec(s))) result.push(temp && temp[1] ? temp[1] : temp[0]);
+    while ((temp = regex.exec(s))) {
+      const item = temp && temp[1] !== void 0 ? temp[1] : temp[0];
+      result.push(item || "");
+    }
     return result;
   },
   get: (from: string[] | null, index: number) => {
